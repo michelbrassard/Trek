40cc42cd0ad68c00456181753fbfe05c127007f3
Michel Brassard <mb6149@rit.edu>
2025-02-04 14:35:04 +0100

    add explanations for registering models

--- backend/trekapi/api/admin.py
+++ backend/trekapi/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import User
 
-# Register your models here.
+# Register your models here so that they appear in the admin site
 admin.site.register(User)
--- backend/trekapi/api/models.py
+++ backend/trekapi/api/models.py
@@ -1,10 +1,15 @@
 from django.db import models # type: ignore - works normally
 
 # Create your models here.
-
 class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.TextField()
     
+    #add for filtering?
     class Meta:
         ordering = ['name']
+        #unique_together = ('name', 'email')
+        """ permissions = [
+            ("can_view_reports", "Can view reports"),
+            ("can_edit_users", "Can edit users"),
+        ] """
