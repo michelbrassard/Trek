df684e99aea2bc175a179cbce6c942f7e80c97da
Michel Brassard <mb6149@rit.edu>
2025-02-06 06:24:24 +0100

    add different button styles

--- frontend/trek-web/src/app/ui/button.tsx
+++ frontend/trek-web/src/app/ui/button.tsx
@@ -2,17 +2,40 @@ import clsx from 'clsx';
 import React from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    isPrimary?: boolean,
+    isSecondary?: boolean,
+    isDanger?: boolean,
+    isDisabled?: boolean,
     children: React.ReactNode;
 }
 
-export function Button({children, className, ...rest}: ButtonProps) {
+export function Button({
+    children, 
+    className, 
+    isPrimary = false, 
+    isSecondary = false, 
+    isDanger = false,
+    isDisabled = false, ...rest}: ButtonProps) {
+
+    const baseStyles = 'flex items-center py-2 px-4 rounded-xl text-base font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 aria-disabled:cursor-not-allowed aria-disabled:opacity-50';
+    const primaryStyles = 'bg-blue-500 hover:bg-blue-400 focus-visible:outline-blue-500 active:bg-blue-600'
+    const secondaryStyles = 'bg-gray-500 hover:bg-gray-400 focus-visible:outline-gray-500 active:bg-gray-600 '
+    const dangerStyles = 'bg-red-500 hover:bg-red-400 focus-visible:outline-red-500 active:bg-red-600'
+    const disabledStyles = 'opacity-50 cursor-not-allowed'
+
     return (
         <button
             {...rest}
             className={clsx(
-                'flex items-center py-1 px-3 rounded-md bg-blue-500 text-base font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+                baseStyles,
+                isPrimary && primaryStyles,
+                isSecondary && secondaryStyles,
+                isDanger && dangerStyles,
+                isDisabled && disabledStyles,
                 className
             )}
+            disabled={isDisabled}
+            //onClick={!isDisabled ? onClick : undefined}
         >
             {children}
         </button>
