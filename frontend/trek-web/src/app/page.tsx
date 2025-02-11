65f1fb5932980cd88e35192e059786c87890440d
Michel Brassard <mb6149@rit.edu>
2025-02-11 21:14:08 +0100

    modify home page to import components

--- frontend/trek-web/src/app/page.tsx
+++ frontend/trek-web/src/app/page.tsx
@@ -3,12 +3,7 @@ import SideNav from "./ui/dashboard/sidenav";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <nav className="flex flex-row gap-4">
-
-      </nav>
-      
-      <main className="flex flex-col gap-8 row-start-2 items-center">
+    <main className="flex-1">
         <Button isPrimary={true}>Primary</Button>
         <Button isSecondary={true}>Secondary</Button>
         <Button isDanger={true}>Danger</Button>
@@ -16,9 +11,5 @@ export default function Home() {
 
         <SideNav />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        Placeholder footer
-      </footer>
-    </div>
   );
 }
