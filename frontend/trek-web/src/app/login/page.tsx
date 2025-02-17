0bd9f996cf57bdc3c0610170171377a25f04eb5b
Michel Brassard <mb6149@rit.edu>
2025-02-17 10:04:11 +0100

    fix login colors

--- frontend/trek-web/src/app/login/page.tsx
+++ frontend/trek-web/src/app/login/page.tsx
@@ -10,9 +10,9 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <main className="flex-1 flex flex-col items-center justify-center md:h-screen p-3">
-      <div className="bg-neutral-900 p-2 rounded-2xl md:w-[400px] w-full">
+      <div className="bg-neutral-100 dark:bg-neutral-900 p-2 rounded-2xl md:w-[400px] w-full">
         <Link href="/">
-          <div className='hover:bg-neutral-800 transition-colors w-fit p-2 rounded-xl'>
+          <div className='hover:bg-neutral-200 hover:dark:bg-neutral-800  transition-colors w-fit p-2 rounded-xl'>
             <ArrowLeft size={20}/>
           </div>
         </Link>
