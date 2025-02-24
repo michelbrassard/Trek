fedf88df0459152bd482ff6ad7d3f0c9eba3e46d
Michel Brassard <mb6149@rit.edu>
2025-02-24 19:05:51 +0100

    modify admin page to view the names and roles of the users

--- backend/trekapi/api/admin.py
+++ backend/trekapi/api/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin # type: ignore (it works)
 from .models import User
+from django.urls import path
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role')  
+    search_fields = ['email', 'first_name', 'last_name'] 
+    list_filter = ('role',) 
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.all()
 
 # Register your models here so that they appear in the admin site
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
