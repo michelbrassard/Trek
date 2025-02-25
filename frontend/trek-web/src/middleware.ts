147a23f455a97a03283b0d6de56512f19b711630
Michel Brassard <mb6149@rit.edu>
2025-02-25 12:49:16 +0100

    add initial middleware to protect pages

---
+++ frontend/trek-web/src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    // const authCheck = await fetch("https://your-django-backend.com/api/auth-status/", {
+    //   headers: { Cookie: req.headers.get("cookie") || "" },
+    // });
+  
+    // if (authCheck.status !== 200) {
+    //   return NextResponse.redirect(new URL("/login", req.url));
+    // }
+  
+    //return NextResponse.next();
+    console.log("Middleware Executed!")
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  
+  // Define which routes to protect
+  export const config = {
+    //probably also had to add the regular "/dashboard"
+    matcher: ["/dashboard/:path*", "/dashboard"],
+  };
