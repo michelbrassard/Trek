235f7d7e61aa78c215e081039bd3085f412b6753
Michel Brassard <mb6149@rit.edu>
2025-02-25 18:39:48 +0100

    remove buttons from the home page

--- frontend/trek-web/src/app/page.tsx
+++ frontend/trek-web/src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "./ui/button";
 import Footer from "./ui/footer";
 import Navigation from "./ui/navigation";
 
@@ -6,12 +5,12 @@ export default function Home() {
   return (
       <div className="flex flex-col h-screen">
         <Navigation />
-        <main className="flex-1">
-          <Button isPrimary={true}>Primary</Button>
-          <Button isSecondary={true}>Secondary</Button>
-          <Button isDanger={true}>Danger</Button>
-          <Button isDisabled={true}>Disabled</Button>
-      </main>
+        <main className="flex-1 p-3 flex justify-center">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold m-1">Trek</h1>
+            <p>This is an athlete dashboard...</p>
+          </div>
+        </main>
         <Footer />
       </div>
   );
