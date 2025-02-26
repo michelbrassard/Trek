1a371125a361394d350a455f779a9920d05f3de1
Michel Brassard <mb6149@rit.edu>
2025-02-26 11:38:54 +0100

    for now leave middleware as is

--- frontend/trek-web/src/middleware.ts
+++ frontend/trek-web/src/middleware.ts
@@ -2,17 +2,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-    // const authCheck = await fetch("https://your-django-backend.com/api/auth-status/", {
-    //   headers: { Cookie: req.headers.get("cookie") || "" },
-    // });
-  
-    // if (authCheck.status !== 200) {
-    //   return NextResponse.redirect(new URL("/login", req.url));
-    // }
-  
-    //return NextResponse.next();
-    console.log("Middleware Executed!")
-    //return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.next();
   }
   
   // Define which routes to protect
