494db29ef2fe9149987b4ed7e44d6512dbe81d96
Michel Brassard <mb6149@rit.edu>
2025-02-26 11:40:42 +0100

    add console log alert when middleware runs

--- frontend/trek-web/src/middleware.ts
+++ frontend/trek-web/src/middleware.ts
@@ -2,6 +2,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
+    console.log("Dashboard accessed.")
     return NextResponse.next();
   }
   
