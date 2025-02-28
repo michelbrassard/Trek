cb4b32d2f6c16cf6dc128069eecf87e6bdb48ab3
Michel Brassard <mb6149@rit.edu>
2025-02-28 16:40:50 +0100

    modify serializer to include everything, but the id and password

--- backend/trekapi/api/serializers.py
+++ backend/trekapi/api/serializers.py
@@ -4,13 +4,4 @@ from api.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 
-                  'created', 
-                  'first_name', 
-                  'last_name', 
-                  'username', 
-                  'email', 
-                  'password', 
-                  'phone', 
-                  'date_of_birth', 
-                  'role']
+        exclude = ['id', 'password']
