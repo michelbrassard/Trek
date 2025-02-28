a71d042090e716e57b49467a7947fb328067e2f7
Michel Brassard <mb6149@rit.edu>
2025-02-28 16:39:43 +0100

    add team and temporary coach codes table + separate models into multiple files

---
+++ backend/trekapi/api/models/team.py
@@ -0,0 +1,16 @@
+import uuid
+from django.db import models
+from datetime import timedelta
+from django.utils.timezone import now
+from .user import User
+
+class TemporaryCoachCode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    coachID = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
+
+
+class Team(models.Model):
+    coachID = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id', related_name="coached_teams")
+    athleteID = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id', related_name="athlete_teams")
+    createdAt = models.DateTimeField(auto_now_add=True)
