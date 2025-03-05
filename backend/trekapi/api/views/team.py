991d35bcc4075272a39210f886b8b074f317c0fa
Michel Brassard <mb6149@rit.edu>
2025-03-05 19:22:23 +0100

    remove unnecesary import

--- backend/trekapi/api/views/team.py
+++ backend/trekapi/api/views/team.py
@@ -1,7 +1,7 @@
 from api.models import User, TemporaryCoachCode
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 from api.serializers import TemporaryCoachCodeSerializer
 
