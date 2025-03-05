b1c6a874cae90967f12acda3ff1d87346b68e3f4
Michel Brassard <mb6149@rit.edu>
2025-03-05 08:58:45 +0100

    add route redirection in middleware

--- frontend/trek-web/src/middleware.ts
+++ frontend/trek-web/src/middleware.ts
@@ -1,12 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */ //REMOVE THIS LATER
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-    console.log("Dashboard accessed.")
-    return NextResponse.next();
+  const token = request.cookies.get("access_token")  
+
+  if(!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
+  return NextResponse.next();
+}
   
-  // Define which routes to protect
   export const config = {
     //probably also had to add the regular "/dashboard"
     matcher: ["/dashboard/:path*", "/dashboard"],
