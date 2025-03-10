69924b149a44a0aa4f9e33384c870697a037eab9
Michel Brassard <mb6149@rit.edu>
2025-03-10 20:41:42 +0100

    create table for viewing athletes

--- frontend/trek-web/src/app/ui/athletes/table.tsx
+++ frontend/trek-web/src/app/ui/athletes/table.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import axios from "axios";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 interface AthletesRowProps {
@@ -35,18 +37,35 @@ export default function Table() {
       }, []);
     
     //TODO: make it a better table and view the overview of each athlete
+    const cellStyling = "border-t border-collapse px-4 py-2 border-neutral-300 dark:border-neutral-700 text-left";
+    const headerCellStyling = "px-4 py-2 text-left"
     return(
-        <div>
+        <div className="my-5 rounded-md overflow-hidden border border-neutral-300 dark:border-neutral-700">
             {tableData ? 
-            <div>
-                {tableData.map((athlete) => (
-                    <div key={athlete.id} className="flex row gap-3">
-                        <p>{athlete.first_name}</p>
-                        <p>{athlete.last_name}</p>
-                        <p>{athlete.email}</p>
-                    </div>
-                ))}
-            </div> 
+            <table className="w-full border-collapse border-neutral-300 dark:border-neutral-700">
+                <thead className="dark:bg-neutral-900 bg-neutral-100">
+                    <tr>
+                        <th className={headerCellStyling}>First Name</th>
+                        <th className={headerCellStyling}>Last Name</th>
+                        <th className={headerCellStyling}>Email</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map((athlete) => (
+                        <tr key={athlete.id}>
+                            <td className={cellStyling}>{athlete.first_name}</td>
+                            <td className={cellStyling}>{athlete.last_name}</td>
+                            <td className={cellStyling}>{athlete.email}</td>
+                            <td className={cellStyling}>
+                                <Link href="/" className="float-end flex items-center gap-2 text-blue-500">
+                                    Details <ArrowRight size={16} />
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table> 
             : "Loading..."}
         </div>
     );
