c68e5ac063c06f225cf6f3188bc91fdb3c813740
Michel Brassard <mb6149@rit.edu>
2025-03-15 17:13:39 +0100

    fix ui for new workout button

--- frontend/trek-web/src/app/dashboard/workouts/page.tsx
+++ frontend/trek-web/src/app/dashboard/workouts/page.tsx
@@ -1,12 +1,17 @@
 import Title from "@/app/ui/dashboard/title";
 import WorkoutsTable from "@/app/ui/workouts/table";
+import { Plus } from "lucide-react";
 import Link from "next/link";
 
 export default function Workouts() {
     return (
         <div>
             <Title text="Workouts"/>
-            <Link href={'workouts/create'}>New</Link>
+            <Link href={'workouts/create'} >
+                <div className="flex items-center gap-2 text-blue-500">
+                    <Plus size={16}/> New
+                </div>
+            </Link>
             <WorkoutsTable />
         </div>
     )
