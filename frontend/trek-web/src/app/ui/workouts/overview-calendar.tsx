713b56ebb869a9c48e9b31e28b9f6672efb5c516
Michel Brassard <mb6149@rit.edu>
2025-05-10 12:34:04 +0200

    fix styling in calendar

--- frontend/trek-web/src/app/ui/workouts/overview-calendar.tsx
+++ frontend/trek-web/src/app/ui/workouts/overview-calendar.tsx
@@ -56,8 +56,8 @@ export default function WorkoutCalendar({ currentDate = new Date() }) {
     const monthDays = generateCalendarMonthDays(currentDate);
 
     return(
-        <div className='relative mt-5'>
-            <div className="grid grid-cols-7 bg-neutral-100 dark:bg-neutral-900 rounded-xl">
+        <div className='relative mt-5 flex flex-row gap-4'>
+            <div className="w-full grid grid-cols-7 bg-neutral-100 dark:bg-neutral-900 rounded-xl">
                 {week.map((day) => (
                     <div key={day} className="text-center px-4 py-3">
                         {day}
@@ -68,9 +68,17 @@ export default function WorkoutCalendar({ currentDate = new Date() }) {
                 {monthDays.map((day, idx) => (
                     <div
                         key={idx}
-                        className={clsx('m-1 h-28 p-2 text-sm transition-all rounded-xl hover:cursor-pointer', 
-                            (isSameMonth(day, currentDate) ? 'text-white bg-neutral-800 hover:dark:bg-neutral-700' : 'text-neutral-500 hover:outline outline-neutral-300 dark:outline-neutral-700'),
-                            (isSameDay(day, currentDate) && 'dark:bg-blue-800 bg-blue-200 hover:dark:dark:bg-blue-700 hover:bg-blue-300')
+                        className={clsx('m-1 h-28 p-2 text-sm transition-all rounded-xl hover:cursor-pointer dark:text-white overflow-scroll', 
+                            (isSameMonth(day, currentDate) ? 
+                                (isSameDay(day, currentDate) ? 
+                                    'bg-blue-200 dark:bg-blue-800 hover:dark:bg-blue-700 hover:bg-blue-300'
+                                    : 
+                                    'bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-800 hover:dark:bg-neutral-700')
+                                : 
+                                'text-neutral-500 hover:outline outline-neutral-300 dark:outline-neutral-700'
+                            ),
+                            (isSameDay(day, selectedDate ?? '') && 'outline outline-blue-500')
+                            
                         )}
                         onClick={() => setSelectedDate(day)}
                     >
@@ -82,7 +90,7 @@ export default function WorkoutCalendar({ currentDate = new Date() }) {
                             .filter((workout => isSameDay(new Date(workout.date), day)))
                             .map(filteredWorkout => 
                                 <div key={filteredWorkout.id} 
-                                    className='max-w-[130px] border-l-2 my-1 pl-2'
+                                    className='max-w-[130px] border-l-2 border-blue-500 my-1 pl-2'
                                 >
                                     <p className='truncate'>{filteredWorkout.title}</p>
                                 </div>
@@ -93,38 +101,43 @@ export default function WorkoutCalendar({ currentDate = new Date() }) {
             <AnimatePresence>
                 {selectedDate && (
                     <motion.div
-                        className="backdrop-blur-md bg-neutral-100/60 dark:bg-neutral-900/60 w-[40%] absolute z-10 right-0 top-1 h-fit border border-neutral-200 dark:border-neutral-800 rounded-2xl mx-2 py-3 px-3"
-                        initial={{ opacity: 0, x: 30 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        exit={{ opacity: 0, x: 30 }}
+                        className="backdrop-blur-md bg-neutral-100 dark:bg-neutral-900/60 w-[40%] rounded-2xl py-3 px-3"
+                        initial={{ x: 50, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        exit={{ x: 50, opacity: 0 }}
                         transition={{ duration: 0.2 }}
                     >
                         <div className='flex flex-row justify-between items-center mb-3'>
                             <h1 className='font-bold text-2xl'>{format(selectedDate, 'PPP')}</h1>
                             <Button 
-                                isSecondary={true}
+                                isDanger={true} isFilled={true}
                                 onClick={() => setSelectedDate(null)}
                             >
                                 Close <X size={22} />
                             </Button>
                         </div>
                         <div className='flex flex-col gap-2 mt-2'>
-                            <Button isPrimary={true}>
+                            <Button isPrimary={true} isFilled={true}>
                                 <Dumbbell size={16} />
                                 Add Workout
                             </Button>
                         </div>
-                        <div className='flex flex-col gap-2 mt-4'>
+                        <div className='flex flex-col gap-2 mt-4 overflow-scroll h-[510px] rounded-md'>
                             {workouts
                             .filter((workout => isSameDay(new Date(workout.date), selectedDate)))
                             .map(filteredWorkout => 
-                                <Link key={filteredWorkout.id} href={`/dashboard/workouts/${filteredWorkout.id}`} 
-                                    className='bg-neutral-800 hover:bg-blue-700 p-2 rounded-lg transition-all'
+                                <motion.div key={filteredWorkout.id}
+                                    initial={{ opacity: 0}}
+                                    animate={{ opacity: 1}}
+                                    transition={{ duration: 0.2 }}
+                                    className='bg-white hover:bg-blue-300 dark:bg-black hover:dark:bg-blue-700 p-2 rounded-lg transition-all'
                                 >
-                                    <p>{filteredWorkout.title}</p>
-                                    <p className='text-sm text-neutral-500'>{filteredWorkout.description}</p>
-                                    <p className='text-sm text-neutral-500'>{filteredWorkout.length} {filteredWorkout.unit}</p>
-                                </Link>
+                                    <Link href={`/dashboard/workouts/${filteredWorkout.id}`}>
+                                        <p className='font-semibold'>{filteredWorkout.title}</p>
+                                        <p className='text-sm text-neutral-500'>{filteredWorkout.description}</p>
+                                        <p className='text-sm text-neutral-500'>{filteredWorkout.length} {filteredWorkout.unit}</p>
+                                    </Link>
+                                </motion.div>
                         )}
                         </div>
                     </motion.div>
