348ea3cfd9c6ae26bd79a7508b777b4316c55aaa
Michel Brassard <mb6149@rit.edu>
2025-05-10 21:59:36 +0200

    add note and resource models

---
+++ backend/trekapi/api/models/resource.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .user import User
+import uuid
+
+class Resource(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    creatorId = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id', related_name="creator_resources")
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    url = models.URLField()
