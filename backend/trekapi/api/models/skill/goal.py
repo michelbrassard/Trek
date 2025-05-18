aefdef04d4f75cd62f968eeb6f28ccfd4d98d695
Michel Brassard <mb6149@rit.edu>
2025-05-18 21:41:42 +0200

    fix foreign key model

--- backend/trekapi/api/models/skill/goal.py
+++ backend/trekapi/api/models/skill/goal.py
@@ -1,9 +1,9 @@
 from django.db import models
-from ..user import User
+from .skill import Skill
 import uuid
 
 class Goal(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    skillId = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id', related_name="skill_goals")
+    skillId = models.ForeignKey(Skill, on_delete=models.CASCADE, to_field='id', related_name="skill_goals")
     title = models.CharField(max_length=50)
     description = models.TextField()
