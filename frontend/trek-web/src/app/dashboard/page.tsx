22cb1fa0254f75907ea19be8e4903c1e5ebc0122
Michel Brassard <mb6149@rit.edu>
2025-06-27 20:51:05 +0200

    create first version of editor that renders editor + preview mode separately

--- frontend/trek-web/src/app/dashboard/page.tsx
+++ frontend/trek-web/src/app/dashboard/page.tsx
@@ -1,13 +1,62 @@
+"use client" //For now!!!
+
+import { ReactNode, useState } from "react";
 import Title from "../ui/dashboard/title";
 import TestModelFastApi from "../ui/DELETEME/test";
+import DotMDEditor from "../ui/form/dotmd-editor";
 
 export default function Overview() {
+
+  const [text, setText] = useState("");
+
+  const parseMDToHTML = (mdText: string): ReactNode => {
+    const lines = mdText.split("\n")
+    
+    const renderedLines = lines.map((line: string, index: number) => {
+      if (line.trim().startsWith("# ")) {
+        return <h1 key={index} className="text-3xl mt-4 font-bold">{line.replaceAll("#", "").trimStart()}</h1>;
+      }
+      else if (line.trim().startsWith("## ")) {
+        return <h2 key={index} className="text-2xl mt-3 font-bold">{line.replaceAll("#", "").trimStart()}</h2>;
+      }
+      else if (line.trim().startsWith("### ")) {
+        return <h3 key={index} className="text-xl mt-2 font-bold">{line.replaceAll("#", "").trimStart()}</h3>;
+      }
+      else if (line.trim().startsWith("#### ")) {
+        return <h4 key={index} className="text-lg mt-2 font-bold">{line.replaceAll("#", "").trimStart()}</h4>;
+      }
+      else if (line.trim().startsWith("##### ")) {
+        return <h5 key={index} className="text-md mt-1 font-bold">{line.replaceAll("#", "").trimStart()}</h5>;
+      }
+      else if (line.trim().startsWith("###### ")) {
+        return <h6 key={index} className="text-sm mt-1 font-bold">{line.replaceAll("#", "").trimStart()}</h6>;
+      }
+      
+      else {
+        return <p key={index} className="text-neutral-800 dark:text-neutral-200">{line}</p>;
+      }
+    });
+
+    return renderedLines
+  }
 
   return (
     <div>
       <Title text="Overview"/>
       <p>Današnji trening i ostalo...</p>
+      <p className="text-lg font-bold">Testing models? (disabled)</p>
       <TestModelFastApi />
+      
+      <p className="text-lg font-bold">Testing the .md editor</p>
+      <DotMDEditor 
+        label={".md Editor"} 
+        id={"editor"} 
+        name={"editor"} 
+        onChange={(e) => setText(e.target.value)} 
+        value={text}
+        rows={0} 
+      />
+      <div className="whitespace-pre">{parseMDToHTML(text)}</div>
     </div>
   );
 }
---
+++ frontend/trek-web/src/app/ui/form/dotmd-editor.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import clsx from "clsx";
+import { useRef, useLayoutEffect } from 'react';
+
+interface DotMDEditorProps {
+    hasProblems?: boolean,
+    label: string,
+    id: string,
+    alertMessage?: string,
+    value?: string,
+    name: string,
+    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    rows: number
+}
+
+export default function DotMDEditor({hasProblems, name, label, id, alertMessage, value, onChange}: DotMDEditorProps) {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const resizeTextArea = () => {
+        const textarea = textareaRef.current;
+        if (textarea) {
+            textarea.style.height = 'auto';
+            textarea.style.height = `${textarea.scrollHeight + 5}px`;
+        }
+    };
+
+    useLayoutEffect(() => {
+        resizeTextArea()
+    }, [value]);
+    
+    const editorStyle = "px-3 w-full block py-2 focus:outline-none focus:ring-0 placeholder-neutral-500 bg-neutral-100 dark:bg-neutral-900 rounded-xl resize-none" 
+    const labelStyle = "uppercase text-[10px] font-medium text-neutral-500"
+    const alertStyle = "border-red-500 dark:border-red-500"
+
+    return(
+        (<div>
+            <label htmlFor={id} className={labelStyle}>{label}</label>
+            <textarea
+                ref={textareaRef}
+                id={id} 
+                name={name} 
+                value={value ? value : ''} 
+                className={clsx(
+                    editorStyle,
+                    hasProblems && alertStyle
+                )}
+                onInput={resizeTextArea}
+                onChange={onChange}
+                required
+                placeholder={name.charAt(0).toUpperCase() + name.slice(1)}
+            >
+            </textarea>
+            {hasProblems && <p className="text-red-500 text-xs mt-1 ml-1">{alertMessage}</p>}
+        </div>
+        )   
+    );
+}
